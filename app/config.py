32f62a2bc2f022c22b93045b02821b5f2e1dfde2
elhidarinouhayla <elhidarinouhayla@gmail.com>
2025-12-16 22:47:51 +0100

    add config file

---
+++ app/config.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+USER=os.getenv("USER", "DB_User")
+PASSWORD=os.getenv("PASSWORD", "DB_PASSWORD")
+PORT=os.getenv("PORT",5432)
+HOST=os.getenv("HOST", "db")
+DATABASE=os.getenv("DATABASE", "DB_NAME")
+
+
+
+HF_TOKEN=os.getenv("HF_TOKEN")
+SECRET_KEY=os.getenv("SECRET_KEY")
+ALGORITHM=os.getenv("ALGORITHM")
+
+
+GEMINI_API_KEY=os.getenv("GEMINI_API_KEY")
