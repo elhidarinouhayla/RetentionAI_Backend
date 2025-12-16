8efe5cbd38ee71841957aa18d12e7a9ffb827bdf
elhidarinouhayla <elhidarinouhayla@gmail.com>
2025-12-16 22:46:12 +0100

    creation d'endpoint login

--- app/main.py
+++ app/main.py
@@ -39,7 +39,20 @@ def create_user(user:UserCreate, db: session=Depends(get_db)):
 
 
 
+# verifier l'identifiant et retourner token
+@app.post("/login")
+def login(user:UserVerify, db: session=Depends(get_db)):
 
+    db_user = db.query(User).filter(
+        User.username == user.username
+        ).first()
+    
+    if not db_user or not verify_password(user.password,db_user.password):
+        raise HTTPException(status_code=400, detail="username or password incorect")
+    
+    token = create_token(db_user.username)
+
+    return {"token" : token}
 
 
 
