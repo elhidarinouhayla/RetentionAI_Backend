2ea2ccbde1735039bf30581ddd0649159de12f51
elhidarinouhayla <elhidarinouhayla@gmail.com>
2025-12-17 19:55:54 +0100

    charger le model de prentrainer de ML

---
+++ app/services/predict_service.py
@@ -0,0 +1,38 @@
+import joblib
+import pandas as pd
+
+model = joblib.load("ML/logistic_regression.pkl")
+
+def predict(data):
+
+    df =  pd.DataFrame([data])
+    return model.predict_proba(df)[0][0]
+
+# test = {
+#   "Gender": "Male",
+#   "Age": 32,
+#   "Department": "Sales",
+#   "JobRole": "Sales Executive",
+#   "MonthlyIncome": 4500,
+#   "YearsAtCompany": 5,
+#   "JobSatisfaction": 3,
+#   "WorkLifeBalance": 3,
+#   "OverTime": "Yes",
+#   "BusinessTravel": "Travel_Rarely",
+#   "Education": 3,
+#   "EducationField": "Marketing",
+#   "EnvironmentSatisfaction": 3,
+#   "JobInvolvement": 3,
+#   "JobLevel": 2,
+#   "MaritalStatus": "Single",
+#   "PercentSalaryHike": 12,
+#   "PerformanceRating": 3,
+#   "RelationshipSatisfaction": 3,
+#   "StockOptionLevel": 1,
+#   "TotalWorkingYears": 8,
+#   "YearsInCurrentRole": 3,
+#   "YearsWithCurrManager": 2
+# }
+
+# print(predict(test))
+
