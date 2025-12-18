6851ae3c62554667551d20c02bdc1000b8da92a1
elhidarinouhayla <elhidarinouhayla@gmail.com>
2025-12-18 15:17:22 +0100

    changer les variabless de la fonction

--- app/services/gemini_service.py
+++ app/services/gemini_service.py
@@ -4,22 +4,29 @@ from app.models.schemas import output_gemini
 
 
 
-def retention_gemini(prompt):
+def retention_gemini(probability, prediction):
 
 
     client = genai.Client(api_key=GEMINI_API_KEY)
 
-    instruction = f"""
+    prompt = f"""
     Contexte : ce salarié a un risque élevé de "churn_probability" de départ selon le modèle ML.  
 
     Tache : propose 3 actions concrètes et personnalisées pour le retenir dans l'entreprise,
     en tenant compte de son role, sa satisfaction, sa performance et son équilibre vie professionnelle/personnelle.  
     Rédige les actions de façon claire et opérationnelle pour un manager RH.
+
+    exemple:
+     "retention_plan": [
+                    "Proposer 2 jours de télétravail",
+                    "Réévaluer la charge de déplacement",
+                    "Plan de formation personnalisé"
+                ]
     """
     
     response = client.models.generate_content(
     model="gemini-2.5-flash",
-    contents=instruction,
+    contents=prompt,
     config={
         "response_mime_type": "application/json",
         "response_json_schema": output_gemini.model_json_schema()
